12639bb126460a70d30633763e1f3b83e8a365ea
Rustam <rustamelutingol@gmail.com>
2017-02-26 14:40:39 +0300

    done

--- homeworks/simple_tasks/bin/02.pl
+++ homeworks/simple_tasks/bin/02.pl
@@ -24,13 +24,20 @@ run(1, 4) - печатает "2\n" и "3\n"
 
 sub run {
     my ($x, $y) = @_;
+    my $bool;
+    $x = 2 if ($x <= 1);
     for (my $i = $x; $i <= $y; $i++) {
+        $bool = 1;
 
-        # ...
-        # Проверка, что число простое
-        # ...
+        for (my $divider = 2; $divider <= sqrt($i) ; $divider++ ){
+            if ( ($i % $divider) == 0){
+                $bool = 0;
+                last;
+            }
+        }
+        
+        if ($bool) {print "$i\n"}
 
-	print "$i\n";
     }
 }
 
