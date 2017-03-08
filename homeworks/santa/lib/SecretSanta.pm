a3f1d447aad4f0f7baa5f368dac606e5cfcbaaae
RustamSultanov <rustamelutingol@gmail.com>
2017-03-08 22:24:35 +0300

    Done

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -7,43 +7,40 @@ use DDP;
 use List::Util qw(first);
 
 sub calculate {
-	#say "CALCULATE";
 	my @members = @_;
 	my @res;
 
-	# get all names
+	# get all names members
 	my @names = map {
 		ref() ? @$_ : $_
 	} @members;
 
 	die "There won't be any surprise to @names" if ($#names <= 2);
 	
-	# hash of hashes that realise logic of relations 
 	my %cantToGive;
 
-	@cantToGive{@names} = map { {$_ => '1'} } @names; # cause smb cant give present himself 
+	@cantToGive{@names} = map { {$_ => '1'} } @names; # cause smb can't give present for himself 
 	
-	foreach my $arrRef (@members) {		#cause husb cant give present his wife and vice versa
+	foreach my $arrRef (@members) {		#cause husband can't give present his wife and vice versa
 		if (ref $arrRef) {				#checking arrays
 			if (ref $arrRef eq 'ARRAY' && scalar(@$arrRef) == 2) {
 			
-				my ($husb, $wife) = @$arrRef; # or $wife, $husb -- no matter
+				my ($husband, $wife) = @$arrRef; # or $wife, $husband -- no matter
 
-				$cantToGive{$husb}{$wife} = 1;
-				$cantToGive{$wife}{$husb} = 1;				
+				$cantToGive{$husband}{$wife} = 1;
+				$cantToGive{$wife}{$husband} = 1;				
 
 			} else {
-				die "Bad data: $arrRef"
+				die "No validate data in $arrRef"
 			}
 		}
 	}
-	#p %cantToGive;	
-	# making random pairs, and adding each of them to %cantToGive
+	
+	# we making random pairs, and adding each of them to has %cantToGive
 
 	my ($from, $to, $toInd);
 
-	# adding each person to %cantToGive{$to}{$everybody} is very slow
-	# its better to create new hash that marks persons with present
+	
 	my %withGift;
 
 	for my $index (0..$#names) {
@@ -55,25 +52,22 @@ sub calculate {
 		$toInd = int(rand(scalar(@names)));	#get random '$to'
 		$to = $names[$toInd];
 
-		# если нет подарка у последнего человека, то его бросили
-		# если у него есть подарок, то из остальны есть только 1 человек без подарка
-		# нужно узнать, может ли последний подарить этому подарок
-		# если да, то дарим и завершаем работу. если нет -- то перевызываем себя(мб exec??)
+		
 		if (exists $withGift{$to} || isForbiddenToGive($from, $to, \%cantToGive)) { 
 			
 			#say grep { ! exists $withGift{$_} &&  $cantToGive{$from}{$_} } @names;
 
 			if ($index == $#names) {
 				
-				if (!(exists $withGift{$from}) 				# 	если последнего обделили и он без подарка, то пиши пропало, некому ему дарить свой подарок
-												||			# 	или
-					(first 	{!exists $withGift{$_} 			#	если среди @names найдется без подарка 
-												&& 			#	и 
-							$cantToGive{$from}{$_} 			#	наш последний $from не может ему ничего подарить
-							}			@names[0..$#names-1])) { #  => пересчет
+				if (!(exists $withGift{$from}) 				# 	if last member without present nobody can't give present for him
+												||			
+					(first 	{!exists $withGift{$_} 			#	if in @names found member without present
+												&& 			 
+							$cantToGive{$from}{$_} 			#	last  $from can't give present for him
+							}			@names[0..$#names-1])) { #  recalculation
 				
 					# tail goto
-					goto &calculate;				#if bad distribution, do it again
+					goto &calculate;				#if bad distribution, do it again!
 
 				} else { 
 					$to = first { !exists $withGift{$_} } @names;
