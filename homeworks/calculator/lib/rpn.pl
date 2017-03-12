1ec822cef556b5b8b62209d368c6333a5ef6bc4c
RustamSultanov <rustamelutingol@gmail.com>
2017-03-12 19:21:12 +0300

    something done

--- homeworks/calculator/lib/rpn.pl
+++ homeworks/calculator/lib/rpn.pl
@@ -21,14 +21,61 @@ no warnings 'experimental';
 use FindBin;
 require "$FindBin::Bin/../lib/tokenize.pl";
 
+sub prior($){
+    my $ops=shift;
+    given ($ops){
+        when (['U+','U-']) {return 4;}
+        when (['^']){return 3;}
+        when (['*','/']){return 2;}
+        when (['+','-']){return 1;}
+        default{return 0;}
+    }
+}
+
+
 sub rpn {
 	my $expr = shift;
 	my $source = tokenize($expr);
 	my @rpn;
+	my @ops = ();
 
-	# ...
-
+	for my $r (@{$source}){
+      given ($r){
+          when (['U+','U-','+','-','*','/','^']){
+              if ($r=~/U[\+\-]|\^/){
+                  while (prior($r)<prior($ops[-1])){
+                      push(@rpn,pop(@ops));
+                  }
+              }
+              else{
+                  while (prior($r)<=prior($ops[-1])){
+                      push(@rpn,pop(@ops));
+                  }
+              }
+              push(@ops,$r);
+          }
+          when('('){
+              push(@ops,$r); 
+          }
+          when(')'){
+              while($ops[-1] ne '('){
+                  if (@ops){push(@rpn,pop(@ops));}
+                  else {die "Something wrong brackets ";}
+              }
+              pop(@ops);
+          }
+          default{
+              $r = 0+$r;
+              push(@rpn,"$r");
+          }
+      }
+  }
+  while (@ops and $ops[-1] ne '('){
+      push(@rpn,pop(@ops));
+  }
+  if (@ops) {die "Bad expression @{$source}";}
 	return \@rpn;
 }
+
 
 1;
--- homeworks/calculator/lib/tokenize.pl
+++ homeworks/calculator/lib/tokenize.pl
@@ -28,9 +28,76 @@ no warnings 'experimental';
 sub tokenize {
 	chomp(my $expr = shift);
 	my @res;
-
-	# ...
-
+	my @chunks = split m{((?<!e)[-+]|[*()/^]|\s+)}, $expr;
+	my $lastToken = 0;
+	my $count = 0;
+	my $prev = '';
+	for my $arg (@chunks) {
+		given ($arg) {
+			when (/^\s*$/) {} 
+			when (/\d/) { 
+				if (/^\d*\.?\d+([e][+-]?\d+)?$/) {
+					$arg = 0 + $arg;
+					push @res, "$arg";
+					$prev = $arg;
+					$lastToken = 1;
+				} else {
+					die "Bad: '$_'";
+				}
+			}
+			when ([ '+','-' ]){ 
+				if (($prev eq ')') || ($prev =~ /\d/)) {
+					push @res, $arg;
+					$prev = $arg;
+				} else {
+					$prev = 'U'.$arg;
+					push @res, $prev;
+				}
+				$lastToken = 0;
+			}
+			when ([ '*','/' ]){
+				if (($prev eq ')') || ($prev =~ /\d/)) {
+					push @res, $arg;
+					$prev = $arg;
+					$lastToken = 0;
+				} else {
+					die "Error!";
+				}
+			}
+			when ('^') {
+				if (($prev eq ')') || ($prev =~ /\d/)) {
+					push @res, $arg;
+					$prev = $arg;
+					$lastToken = 0;
+				} else {
+					die "Error!";
+				}
+			}
+			when ([ '(',')' ]) {
+					if ($arg eq '(') {
+						$count++;
+					} else {
+						$count--;
+					}
+					if ($prev =~ /\d/) {
+						$lastToken = 1;
+					} else {
+						$lastToken = 0;
+					}
+					push @res, $arg;
+					$prev = $arg;
+			}
+			default {
+				die "Bad: '$_'";
+			}
+		}
+	}
+	if ($count != 0) {
+		die "Error: wrong sequence of brackets!";
+	}
+	if ($lastToken != 1) {
+		die "Bad: tokenize failed, last chunk is not number";
+	}
 	return \@res;
 }
 
